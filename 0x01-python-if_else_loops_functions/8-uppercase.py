9dead92415ddde5301ef3a7007aeb424e2dae7e1
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-09-05 14:11:17 +0000

    Commit of task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord(c) <= 122 and ord(c) >= 97:
-            print("{:c}".format(ord(c) - 32), end="")
-        else:
-            print(c, end="")
+        print("{:c}".format(ord(c) - 32 if ord(c) <= 122 and ord(c) >= 97
+                            else ord(c)), end="")
     print("\n")
