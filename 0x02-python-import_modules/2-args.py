469712ca6211df44a9c35c258f21db62d0f91236
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-09-07 14:40:27 +0000

    Commit of task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    length = len(sys.argv)
+    print("{} arguments{}".format(length - 1, "." if length == 1 else ":"))
+    for i in range(1, length):
+        print("{}: {}".format(i, sys.argv[i]))
