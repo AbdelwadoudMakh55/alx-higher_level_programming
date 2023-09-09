dabb53272ae97767bb43562452dd514a4b0e0841
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-09-09 19:16:05 +0000

    Commit of task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -2,23 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * len_list - Return the length of a singly linked list.
- * @head : Pointer to pointer to head of singly linked list.
- * Return: Len of list.
- */
-int len_list(listint_t *head)
-{
-	int len = 0;
-	listint_t *current = head;
-
-	while (current != NULL)
-	{
-		len++;
-		current = current->next;
-	}
-	return (len);
-}
-/**
  * is_palindrome - checks if a singly linked list is a palindrome.
  * @head : Pointer to pointer to head of singly linked list.
  * Return: 1 if palindrome, 0 if not.
@@ -26,22 +9,30 @@ int len_list(listint_t *head)
 int is_palindrome(listint_t **head)
 {
 	listint_t *current;
-	int len = len_list(*head);
-	int numbers[len], i = 0, result = 1;
+	int len = 0, i = 0, result = 1, *array;
 
 	if (*head == NULL)
 		return (1);
 	current = *head;
+        while (current != NULL)
+	{
+		len++;
+		current = current->next;
+	}
+	array = malloc(len * sizeof(int));
+	if (array == NULL)
+		return (0);
+	current = *head;
 	while (current != NULL)
 	{
-		numbers[i] = (*current).n;
+		array[i] = (*current).n;
 		current = (*current).next;
 		i++;
 	}
 	i = 0;
 	while (i < len / 2)
 	{
-		if (numbers[i] != numbers[len - 1 - i])
+		if (array[i] != array[len - 1 - i])
 		{
 			result = 0;
 			break;
