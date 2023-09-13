5df215efe090ce15f832234cc30dbb34e5fc292a
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-09-13 13:33:54 +0000

    Commit of task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,10 +3,9 @@ def best_score(a_dictionary):
     if isinstance(a_dictionary, dict) is False or not a_dictionary:
         return None
     maxi = list(a_dictionary.values())[0]
+    best = list(a_dictionary.keys())[0]
     for key in a_dictionary.keys():
         if a_dictionary[key] > maxi:
             maxi = a_dictionary[key]
             best = key
-    if isinstance(best, str) is False:
-        return None
     return best
