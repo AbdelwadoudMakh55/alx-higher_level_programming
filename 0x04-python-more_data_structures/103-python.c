15920cd28fd28b56089ab0155749a86e4a986758
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-09-13 22:06:13 +0000

    Commit of task 16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -9,9 +9,23 @@ void print_python_list(PyObject *p)
 	long int i;
 	PyListObject *new;
 
+	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
 	new = (PyListObject *)p;
 	printf("[*] Allocated = %ld\n", new->allocated);
 	for (i = 0; i < PyList_Size(p); i++)
 		printf("Element %ld: %s\n", i, new->ob_item[i]->ob_type->tp_name);
 }
+/**
+ * print_python_bytes - print some basic info about Python bytes objects.
+ * @p : Pointer to PyObject.
+ * Return: Void.
+ */
+/*void print_python_bytes(PyObject *p)
+{
+
+	printf("[.] bytes object info\n");
+	printf("size: %ld\n", );
+	printf("trying string: %s\n", );
+	printf("first %ld bytes: \n", );
+}*/
