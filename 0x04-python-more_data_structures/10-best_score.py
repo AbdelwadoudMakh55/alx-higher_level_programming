6739a11fdfb1cdc863ee69e19372ae54d99dd0d4
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-09-13 13:03:49 +0000

    Commit of task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if isinstance(a_dictionary, dict):
-        max = a_dictionary.values()[0]
+        max = list(a_dictionary.values())[0]
         for key in a_dictionary.keys():
             if a_dictionary[key] > max:
                 max = a_dictionary[key]
