ff4cf63a9c0c55cebb3c766e7e30075c7da8f58c
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-09-13 11:36:18 +0000

    Commit of task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,7 @@ def square_matrix_simple(matrix=[]):
         new_matrix = []
         for i in range(len(matrix)):
             row = []
-            for j in range(len(matrix)):
+            for j in range(len(matrix[i])):
                 row.append(matrix[i][j] ** 2)
             new_matrix.append(row)
         return new_matrix
