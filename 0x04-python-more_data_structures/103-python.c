0da3ede2fcf06915fe337af6988b54312ed72930
AbdelwadoudMakh55 <Abdelwadoud315@gmail.com>
2023-09-13 21:55:23 +0000

    Commit of task 16

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,17 @@
+#include <Python.h>
+/**
+ *  print_python_list_info - prints some basic info about Python lists.
+ *  @p : Pointer to a PyObject.
+ *  Return: Void.
+ */
+void print_python_list(PyObject *p)
+{
+	long int i;
+	PyListObject *new;
+
+	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
+	new = (PyListObject *)p;
+	printf("[*] Allocated = %ld\n", new->allocated);
+	for (i = 0; i < PyList_Size(p); i++)
+		printf("Element %ld: %s\n", i, new->ob_item[i]->ob_type->tp_name);
+}
