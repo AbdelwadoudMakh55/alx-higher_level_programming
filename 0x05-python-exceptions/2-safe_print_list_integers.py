1d6c50142ae970b3e5bf33c441d1cd95c73a17fa
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-09-25 12:04:51 +0000

    Commit of task 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     result = 0
-    try:
-        for i in range(x):
-            try:
-                print("{:d}".format(my_list[i]), end="")
-                result += 1
-            except (ValueError, TypeError):
-                pass
-    except IndexError:
-        print("")
-        return result
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            result += 1
+        except (ValueError, TypeError):
+            pass
     print("")
     return result
