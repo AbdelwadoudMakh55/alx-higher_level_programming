6f15d167d55ba560ee8f46d14971296f27e322cf
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-09-25 11:13:52 +0000

    Commit of task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(x + 1):
+        result = 0
+        for i in range(x):
             print(f"{my_list[i]}", end="")
             result = i + 1
     except IndexError:
