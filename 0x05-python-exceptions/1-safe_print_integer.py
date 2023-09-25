ab55e6503e6cf6d9bab3d2899201a1cbf512ec39
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-09-25 11:27:00 +0000

    Commit of task 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    result = True
     try:
+        result = True
         print("{:d}".format(value))
     except ValueError:
         result = False
