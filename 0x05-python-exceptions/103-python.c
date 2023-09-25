c85bf8f61ca80513024cbceb282bef4dc840e5d3
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-09-25 22:57:24 +0000

    Commit of task 10

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -4,6 +4,7 @@
  *  @p : Pointer to a PyObject.
  *  Return: Void.
  */
+void print_python_float(PyObject *p);
 void print_python_bytes(PyObject *p);
 void print_python_list(PyObject *p)
 {
@@ -19,6 +20,8 @@ void print_python_list(PyObject *p)
 		printf("Element %ld: %s\n", i, new->ob_item[i]->ob_type->tp_name);
 		if (new->ob_item[i]->ob_type->tp_name[0] ==  'b')
 			print_python_bytes((PyObject *)new->ob_item[i]);
+		if (new->ob_item[i]->ob_type->tp_name[0] ==  'f')
+			print_python_float((PyObject *)new->ob_item[i]);
 	}
 }
 /**
@@ -53,4 +56,20 @@ void print_python_bytes(PyObject *p)
 			printf(" %02x", PyBytes_AsString(p)[i]);
 		printf("\n");
 	}
+}
+/**
+ * print_python_float - print some basic info about Python floats objects.
+ * @p : Pointer to PyObject.
+ * Return: Void.
+ */
+void print_python_float(PyObject *p)
+{
+	if (!PyFloat_Check(p))
+	{
+		printf("[.] float object info\n");
+		printf("  [ERROR] Invalid Float Object\n");
+		return;
+	}
+	printf("[.] float object info\n");
+	printf("  value: %f\n", );
 }
