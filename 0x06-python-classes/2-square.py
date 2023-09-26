289003cfc64b68832be9b927373bf448e1fbfe79
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-09-26 12:13:39 +0000

    Commit of task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""This is the 2-square module
+Classes:
+    - 'Square': This the class of squares.
+"""
+
+
+class Square:
+    """ This the square class.
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
