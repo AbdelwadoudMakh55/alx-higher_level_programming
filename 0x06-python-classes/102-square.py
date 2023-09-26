1ec3b0363c1f1748c4cd1e899c16255d58955080
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-09-26 22:05:40 +0000

    Commit of task 9

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""This is the 102-square module
+Classes:
+    - 'Square': This the class of squares.
+"""
+
+
+class Square:
+    """ This the square class.
+    """
+
+    def __init__(self, size=0):
+        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
