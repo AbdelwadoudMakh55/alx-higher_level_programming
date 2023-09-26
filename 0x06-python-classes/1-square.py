58dc9720a5208875334474a6f240c2709c7f94df
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-09-26 11:50:29 +0000

    Commit of task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This is the 1-square module
+Classes:
+    - 'Square': This the class of squares.
+"""
+
+
+class Square:
+    """ This the square class.
+    """
+
+    def __init__(self, size):
+        self.__size = size
