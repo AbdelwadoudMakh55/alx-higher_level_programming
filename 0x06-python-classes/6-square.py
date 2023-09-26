941d6a588d4af81a42d4dd07e2434c4e872cbc3c
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-09-26 17:22:38 +0000

    Commit of task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -58,8 +58,9 @@ class Square:
             print("")
         else:
             for i in range(self.__size):
-                for j in range(self.__position[0]):
-                    print(" ", end="")
+                if (self.__position[1] == 0):
+                    for j in range(self.__position[0]):
+                        print(" ", end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print("")
