87eeabd3a0c432a92169e5344893ddfcdcb46395
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-09-30 23:21:22 +0000

    Commit of task 7

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This the 101-lazy_matrix_mul module
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """ This the lazy_matrix_mul function """
+    return np.matmul(m_a, m_b)
