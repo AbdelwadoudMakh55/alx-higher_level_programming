8e41fe44e7ae27f067eb9453baa5fff13037c151
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-01 18:13:22 +0000

    Commit of singly linked list task

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,6 +3,8 @@
 Classes:
     - 'Node': Class of a singly linked list.
 """
+
+
 class Node:
     """This is the Node class
     """
@@ -10,7 +12,7 @@ class Node:
     def __init__(self, data, next_node=None):
         if not isinstance(data, int):
             raise TypeError("data must be an integer")
-        if not isinstance(next_node, Node) and next_node != None:
+        if not isinstance(next_node, Node) and next_node is not None:
             raise TypeError("next_node must be a Node object")
         self.__data = data
         self.__next_node = next_node
@@ -31,9 +33,10 @@ class Node:
 
     @next_node.setter
     def next_node(self, value):
-        if not isinstance(value, Node) and value != None:
+        if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
+
 
 class SinglyLinkedList:
     """This the SinglyLinkedList class
@@ -44,12 +47,12 @@ class SinglyLinkedList:
 
     def sorted_insert(self, value):
         copy_head = self.__head
-        if self.__head == None:
+        if self.__head is None:
             self.__head = Node(value, None)
         else:
             current = self.__head
             previous = current
-            while current != None and value > current.data:
+            while current is not None and value > current.data:
                 previous = current
                 current = current.next_node
             new_node = Node(value, current)
@@ -57,10 +60,11 @@ class SinglyLinkedList:
                 previous.next_node = new_node
             else:
                 self.__head = new_node
+
     def __str__(self):
         list_str = ""
-        while self.__head != None:
-            if self.__head.next_node == None:
+        while self.__head is not None:
+            if self.__head.next_node is None:
                 list_str += str(self.__head.data)
             else:
                 list_str += str(self.__head.data) + '\n'
