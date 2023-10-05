ae2bdbfa13f9e06f0bb515da4420370972410a9f
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-05 06:14:39 +0000

    Commit of task 5

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -17,20 +17,3 @@ class TestMaxInteger(unittest.TestCase):
         self.assertAlmostEqual(max_integer([1, 5, 7]), 7)
         self.assertAlmostEqual(max_integer([0, 0, 0]), 0)
         self.assertAlmostEqual(max_integer([-2, -4, -3]), -2)
-
-    def test_type(self):
-        """ Test the type of value """
-        self.assertRaises(TypeError, max_integer, "abcd")
-        self.assertRaises(TypeError, max_integer, 5)
-        self.assertRaises(TypeError, max_integer, 5.23)
-        self.assertRaises(TypeError, max_integer, (5, 3))
-        self.assertRaises(TypeError, max_integer, {2, 10})
-        self.assertRaises(TypeError, max_integer, True)
-
-    def test_value(self):
-        """ Test the value """
-        self.assertRaises(ValueError, max_integer, [[5, 6]])
-        self.assertRaises(ValueError, max_integer, ["abcd", 5])
-        self.assertRaises(ValueError, max_integer, [5.36, 5])
-        self.assertRaises(ValueError, max_integer, [(5, 3)])
-        self.assertRaises(ValueError, max_integer, [{1, "abc"}, 11])
