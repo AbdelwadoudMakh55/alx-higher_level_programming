64e9837f202b59a149e9663dfe83eb44867b2df2
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-09 15:22:12 +0000

    Commit of task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This is the 2-is_same_class module """
+
+
+def is_same_class(obj, a_class):
+    """ This is the is_same_class function """
+    if type(obj) is a_class:
+        return True
+    return False
