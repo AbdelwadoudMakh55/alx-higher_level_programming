11551ec782cd8e56e9962ed2c69e5a9d487ced31
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-09 15:28:23 +0000

    Commit of task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" This is the 3-is_kind_of_class module """
+
+
+def is_kind_of_class(obj, a_class):
+    """ This is the is_kind_of_class function """
+    return isinstance(obj, a_class)
