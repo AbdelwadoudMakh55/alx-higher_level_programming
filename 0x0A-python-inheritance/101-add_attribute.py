ada98c5f40dba5152cf5411326ffefe0426775e8
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-09 19:10:26 +0000

    Commit of task 13

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This is the 101-add_attribute module """
+
+
+def add_attribute(obj, att_name, value):
+    result = 0
+    for attr in dir(obj):
+        if attr[0] != '_':
+            result = 1
+            break
+    if result == 0:
+        setattr(obj, att_name, value)
+    else:
+        raise TypeError("can't add new attribute")
