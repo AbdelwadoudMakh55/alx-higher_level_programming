1e7b797f986f164b3306e8708092dd74d068b36c
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-09 19:40:19 +0000

    Commit of task 13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,12 +4,25 @@
 
 def add_attribute(obj, att_name, value):
     """ This is the add_attribute function """
-    result = 0
-    for attr in dir(obj):
-        if attr[0] != '_':
-            result = 1
-            break
-    if result == 0:
-        setattr(obj, att_name, value)
-    else:
+    if hasattr(obj, '__slots__'):
+        if type(type(obj).__slots__) is str:
+            if att_name == type(obj).__slots__:
+                setattr(obj, att_name, value)
+                return
+            else:
+                raise TypeError("can't add new attribute")
+        for attr in type(obj).__slots__:
+            if attr == att_name:
+                setattr(obj, att_name, value)
+                return
         raise TypeError("can't add new attribute")
+    else:
+        result = 0
+        for attr in dir(obj):
+            if attr[0] != '_':
+                result = 1
+                break
+        if result == 0:
+            setattr(obj, att_name, value)
+        else:
+            raise TypeError("can't add new attribute")
