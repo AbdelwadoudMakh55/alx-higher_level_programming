fd5a2d81831ea6f7750854c2c5a97db740203d35
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-10 09:05:43 +0000

    Commit of task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,7 +8,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """ This the save_to_json_file function """
     with open(filename, "w", encoding='utf-8') as f:
-        if type(my_obj) != str:
-            return f.write(json.dumps(my_obj))
-        else:
-            return f.write(my_obj)
+        return f.write(json.dumps(my_obj))
