1ad38a390e17bb5a0812786f946493b4fb59edb3
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-10 21:03:22 +0000

    Commitof task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" this is the 100-append_after module """
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, "r", encoding='utf-8') as f:
+        all_lines = f.readlines()
+        new_lines = []
+        for line in all_lines:
+            if line.find(search_string) != -1:
+                new_lines.append(line)
+                new_lines.append(new_string)
+            else:
+                new_lines.append(line)
+    with open(filename, "w", encoding='utf-8') as f:
+        f.writelines(new_lines)
