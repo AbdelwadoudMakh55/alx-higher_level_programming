88de49e7d7bea3ebfa938a4c3a87294695405acf
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-10 12:23:46 +0000

    Commit of task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" This is the 11-student module """
+
+
+class Student:
+    """ This is the Student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        json_rep = {}
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return vars(self)
+            for attr in attrs:
+                if attr in vars(self).keys():
+                    json_rep[attr] = vars(self)[attr]
+            return json_rep
+        return vars(self)
+
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
