585ed4997d075c4485bcce4853940bcc5fe15e90
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-10 10:47:16 +0000

    Commit of task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,7 @@
 
 import json
 import sys
+from pathlib import Path
 
 
 def save_to_json_file(my_obj, filename):
@@ -11,17 +12,27 @@ def save_to_json_file(my_obj, filename):
     with open(filename, "w", encoding='utf-8') as f:
         return f.write(json.dumps(my_obj))
 
+
 def load_from_json_file(filename):
     """ This the load_from_json_file function """
     with open(filename, "r", encoding='utf-8') as f:
         return json.loads(f.read())
 
+
 def main():
-    if len(sys.argv) == 1:
-        save_to_json_file([], "add_item.json")
-    else:
+    """ This is the main function"""
+    path = Path("./add_item.json")
+    if path.is_file():
         p_list = load_from_json_file("add_item.json")
+        if type(p_list) is not list:
+            save_to_json_file([], "add_item.json")
+            p_list = load_from_json_file("add_item.json")
+    else:
+        save_to_json_file([], "add_item.json")
+    p_list = load_from_json_file("add_item.json")
     for i in range(1, len(sys.argv)):
         p_list.append(sys.argv[i])
         save_to_json_file(p_list, "add_item.json")
+
+
 main()
