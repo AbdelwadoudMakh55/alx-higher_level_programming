b43a5c6157c6f367eb569a02b50bdd2dacf1e6be
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-10 21:04:24 +0000

    Commitof task 13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,6 +3,7 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
+    """ This is the append_after function """
     with open(filename, "r", encoding='utf-8') as f:
         all_lines = f.readlines()
         new_lines = []
