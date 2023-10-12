835eb6df3c19bcb0f54173f600872bbbc012ba70
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-12 22:15:54 +0000

    Commit of task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,6 +11,14 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        Rectangle.width.fset(self, value)
+
     def __str__(self):
         sq_str = f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
         return f"{sq_str}"
