ad2f8cac22aa4c60cc444732cec7a46f90cd689d
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-12 22:34:22 +0000

    Commit of task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,3 +22,12 @@ class Square(Rectangle):
     def __str__(self):
         sq_str = f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
         return f"{sq_str}"
+
+    def update(self, *args, **kwargs):
+        attr = ["id", "size", "x", "y"]
+        if args is not None and len(args) > 0:
+            for i in range(len(args)):
+                setattr(self, attr[i], args[i])
+        else:
+            for key in list(kwargs.keys()):
+                setattr(self, key, kwargs[key])
