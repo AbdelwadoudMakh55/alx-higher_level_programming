ebdb84a895fb5ad60baf410d7a9f151f5f13b0cf
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-10-14 11:28:55 +0000

    Commit of task 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,9 +18,23 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """ This is the to_json_string function """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return []
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ This is the save_to_file function """
+        if list_objs is not None and len(list_objs) != 0:
+            file_name = list_objs[0].__class__.__name__ + ".json"
+        else:
+            file_name = "Base.json"
+        l_dict = []
+        with open(file_name, "w", encoding='utf-8') as f:
+            for objs in list_objs:
+                l_dict.append(vars(objs))
+            f.write(cls.to_json_string(l_dict))
