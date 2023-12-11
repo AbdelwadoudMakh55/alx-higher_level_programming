a71422761f0d5622cc907e16c2f64136d45c0081
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-12-11 13:35:53 +0000

    isNaN()

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv[2]) {
+  if (isNaN(process.argv[2]) === false) {
+    console.log('My number: ' + Number(process.argv[2]));
+  } else {
+    console.log('Not a number');
+  }
+} else {
+  console.log('Not a number');
+}
