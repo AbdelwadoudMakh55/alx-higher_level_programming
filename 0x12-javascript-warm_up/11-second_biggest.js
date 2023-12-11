f9de911a423816dfa47a24dd6587a299d1ea450d
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-12-11 15:40:01 +0000

    Math.max(), arr.splice(), arr.indexOf()

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const array = [];
+let j = 0;
+if (process.argv[2]) {
+  if (process.argv.length === 3) {
+    console.log(0);
+  } else {
+    for (let i = 2; i < process.argv.length; i++) {
+      array[j] = Number(process.argv[i]);
+      j++;
+    }
+    array.splice(array.indexOf(Math.max(...array)));
+    console.log(Math.max(...array));
+  }
+} else {
+  console.log(0);
+}
