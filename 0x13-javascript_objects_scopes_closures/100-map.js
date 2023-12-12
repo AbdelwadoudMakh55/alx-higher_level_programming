b3137464241032b86fe83c26b3756ffbc82ec5ad
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-12-12 16:51:36 +0000

    Map

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./100-data').list;
-const map1 = list.map((x) => x * list.indexOf(x));
-console.log(list);
+const list1 = require('./100-data').list;
+const map1 = list1.map((num, index) => num * index);
+console.log(list1);
 console.log(map1);
