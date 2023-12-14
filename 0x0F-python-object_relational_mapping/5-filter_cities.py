7891089fa22ac8ad299f5811de251adbd277e6af
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-12-14 19:27:39 +0000

    INNER JOIN and user input

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" This is the 5-filter_cities module it lists all the cities
+in a state in hbtn_0e_0_usa database """
+
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    if ';' not in sys.argv[4]:
+        cur.execute("SELECT cities.name FROM states \
+                    INNER JOIN cities ON states.id = cities.state_id \
+                    WHERE states.name = %s \
+                    ORDER BY cities.id", [sys.argv[4]])
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            if query_rows.index(row) != len(query_rows) - 1:
+                print(f"{row[0]}, ", end='')
+            else:
+                print(f"{row[0]}")
+    cur.close()
+    conn.close()
