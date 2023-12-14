2a1715cb41ee91161a9c12a27eeac8ead00fdd87
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-12-14 16:45:27 +0000

    Establishing connection and executing first query

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" This is the 0-select_states module it lists all the states present in the
+hbtn_0e_0_usa database """
+
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
