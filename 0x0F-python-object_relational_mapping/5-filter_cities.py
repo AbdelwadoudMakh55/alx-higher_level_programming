51dba288fb8001713e3c3bbb24cce2f42afa6205
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-12-15 09:05:37 +0000

    Case sensitive filtering using user input

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -14,7 +14,6 @@ if __name__ == '__main__':
         cur.execute("SELECT cities.name FROM states \
                     INNER JOIN cities ON states.id = cities.state_id \
                     WHERE states.name='{0}' \
-                    COLLATE utf8mb4_0900_as_cs \
                     ORDER BY cities.id ASC".format(sys.argv[4]))
         query_rows = cur.fetchall()
         for row in query_rows:
