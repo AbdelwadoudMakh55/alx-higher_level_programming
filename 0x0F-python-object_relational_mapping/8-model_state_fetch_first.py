460a31736f933bb585ab6063e87da20d73942a63
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2023-12-15 18:08:57 +0000

    Querying with ORM

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This is the 8-model_state_fetch_first module """
+
+import sys
+from model_state import Base, State
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    try:
+        f_state = session.query(State).first()
+        print(f"{f_state.id}: {f_state.name}")
+    except NoResultFound:
+        print("Nothing")
