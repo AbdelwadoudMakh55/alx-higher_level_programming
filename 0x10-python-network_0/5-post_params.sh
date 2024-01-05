8917f3e91f0f23cfa24c61dae2842fe53e444834
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-05 13:16:34 +0000

    POST request

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sending a POST request with parameters
-curl -X POST "$1" -H "Content-Type: application/json" -d "{"email": "test@gmail.com", "subject":  "will always be here for PLD"}" -s
+curl -X POST -d "{"email": "test@gmail.com", "subject":  "will always be here for PLD"}"  "$1" -s
