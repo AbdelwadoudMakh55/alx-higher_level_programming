202531a200093ce74a9c7b54b9ad91ad0172ba79
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-05 18:09:00 +0000

    Peak Optimal solution

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,13 +7,7 @@ def find_peak(list_of_integers):
 
     if len(list_of_integers) == 0 or list_of_integers is None:
         return None
-    list_copy = list_of_integers[:]
-    list_copy1 = list_of_integers[:]
-    list_copy.sort()
-    list_copy1.sort(reverse=True)
-    if list_of_integers == list_copy:
-        return list_of_integers[len(list_of_integers) - 1]
-    if list_of_integers == list_copy1:
-        return list_of_integers[0]
     maxi = max(list_of_integers)
-    return maxi
+    for n in list_of_integers:
+        if n >= maxi:
+            return n
