fe20a1480f5aa7b8c9bc562704b4012f3044638c
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-05 19:02:49 +0000

    Peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,7 +7,15 @@ def find_peak(list_of_integers):
 
     if len(list_of_integers) == 0 or list_of_integers is None:
         return None
-    maxi = max(list_of_integers)
-    for n in list_of_integers:
-        if n >= maxi:
-            return n
+    low = 0
+    high = len(list_of_integers) - 1
+    while low < high:
+        middle = low + (high - low) // 2
+        if list_of_integers[middle] >= list_of_integers[middle - 1] and \
+           list_of_integers[middle] >= list_of_integers[middle + 1]:
+            return list_of_integers[middle]
+        elif list_of_integers[middle + 1] > middle:
+            low = middle + 1
+        else:
+            high = middle - 1
+    return None
