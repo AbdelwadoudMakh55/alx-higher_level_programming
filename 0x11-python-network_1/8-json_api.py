3e1385ca1cbfefb51992b75b6b90ab7823767f62
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-06 11:35:14 +0000

    post data

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,8 +10,7 @@ if __name__ == "__main__":
     else:
         q = ""
     r = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
-    if r.json():
-        print(type(r.json()))
+    if r.status_code == 200:
         if len(r.json()) > 0:
             print(f'[{r.json()["id"]}] {r.json()["name"]}')
         else:
