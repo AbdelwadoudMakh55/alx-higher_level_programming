5b0a7ac8142c2ef0e4e291f9f35775244c0855d7
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-06 11:16:58 +0000

    post data

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This is the 8-json_api module """
+
+
+import requests
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+    r = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+    if r.json:
+        print(r.json)
