d5021af57e0c7762f742eea397fc66099a66b038
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-06 08:53:52 +0000

    First request

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,6 +6,6 @@ from urllib import request
 with request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
     content = resp.read()
     print("Body response:")
-    print(f"\t - type: {type(content)}")
-    print(f"\t - content: {content}")
-    print(f"\t - utf8 content: {content.decode()}")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode()}")
