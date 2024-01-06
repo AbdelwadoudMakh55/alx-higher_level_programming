eff5c8cc3b23c44207234e8abb2c733a88daf34a
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-06 10:51:57 +0000

    Exception handling

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -5,7 +5,10 @@
 import requests
 import sys
 if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    r_id = r.headers["X-Request-Id"]
-    if r_id is not None:
-        print(r_id)
+    try:
+        r = requests.get(sys.argv[1])
+        r_id = r.headers["X-Request-Id"]
+        if r_id is not None:
+            print(r_id)
+    except Exception:
+        pass
