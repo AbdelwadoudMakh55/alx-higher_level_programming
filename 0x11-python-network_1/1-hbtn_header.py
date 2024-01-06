938cfd4e0085fcbc112fb247411f97e98d0388bb
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-06 09:31:19 +0000

    Request ID

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,5 +6,6 @@ from urllib import request
 import sys
 if __name__ == "__main__":
     with request.urlopen(sys.argv[1]) as resp:
-        if "_headers" in resp.headers.__dict__:
-            print(resp.headers.__dict__["_headers"][-3][1])
+        for att in resp.headers.__dict__["_headers"]:
+            if att[0] == "X-Request-Id":
+                print(att[1])
