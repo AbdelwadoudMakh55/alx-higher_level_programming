116d2fd484f594bdd24bd3ca8a7ab440d7eeb723
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-06 10:39:45 +0000

    Exception handling

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -3,6 +3,7 @@
 
 
 import requests
+import sys
 if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
+    r = requests.get(sys.argv[1])
     print(r.headers["X-Request-Id"])
