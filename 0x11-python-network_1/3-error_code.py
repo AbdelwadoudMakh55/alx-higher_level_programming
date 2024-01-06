09b3dbd48275fa068a32fa3e7fabee328ac8815a
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-06 10:23:46 +0000

    Exception handling

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,12 +2,12 @@
 """ This is the 3-error_code module """
 
 
-import urllib
+from urllib import request
 import sys
 if __name__ == "__main__":
     import urllib.error
     try:
-        with urllib.request.urlopen(sys.argv[1]) as req:
-            print(req.read())
+        with request.urlopen(sys.argv[1]) as req:
+            print(req.read().decode())
     except urllib.error.URLError as e:
         print(f'Error code: {e.reason}')
