6097ae6fa1819d838de18940b81577cd3090acdc
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-30 12:57:14 +0000

    Star wars characterID 18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,22 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  const epList = JSON.parse(body).results;
-  let charID18 = 0;
-  for (let i = 0; i < epList.length; i++) {
-    const chL = epList[i].characters;
-    for (let j = 0; j < chL.length; j++) {
-      if (chL[j].slice(43, chL[j].length - 1) === '18') {
-        charID18++;
+const url = 'https://swapi-api.alx-tools.com/api/films';
+if (process.argv[2] === url) {
+  request(process.argv[2], function (error, response, body) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    const epList = JSON.parse(body).results;
+    let charID18 = 0;
+    for (let i = 0; i < epList.length; i++) {
+      const chL = epList[i].characters;
+      for (let j = 0; j < chL.length; j++) {
+        if (chL[j].slice(43, chL[j].length - 1) === '18') {
+          charID18++;
+        }
       }
     }
-  }
-  console.log(charID18);
-});
+    console.log(charID18);
+  });
+}
