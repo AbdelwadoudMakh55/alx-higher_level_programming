bb8119ddde8b46ce368d0743f72a07c578f53617
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-01-30 14:00:05 +0000

    Character id 18

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,19 @@
 #!/usr/bin/node
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  const epList = JSON.parse(body).results;
-  let charID18 = 0;
-  if (epList !== undefined) {
-    for (let i = 0; i < epList.length; i++) {
-      const chL = epList[i].characters;
-      for (let j = 0; j < chL.length; j++) {
-        if (chL[j].slice(43, chL[j].length - 1) === '18') {
-          charID18++;
+  if (!error) {
+    const epList = JSON.parse(body).results;
+    let charID18 = 0;
+    if (epList !== undefined) {
+      for (let i = 0; i < epList.length; i++) {
+        const chL = epList[i].characters;
+        for (let j = 0; j < chL.length; j++) {
+          if (chL[j].slice(43, chL[j].length - 1) === '18') {
+            charID18++;
+          }
         }
       }
     }
+    console.log(charID18);
   }
-  console.log(charID18);
 });
