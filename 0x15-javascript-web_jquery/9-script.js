9a1e813f6b8d2e073d99355804b4e9043819a2f4
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-02-07 14:28:12 +0000

    Hands on JQuery

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,7 @@
 const $ = window.$;
 const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
-$.get(url, function (data) {
-  $('#hello').html(data.hello);
+$(document).ready(function () {
+  $.get(url, function (data) {
+    $('#hello').html(data.hello);
+  });
 });
